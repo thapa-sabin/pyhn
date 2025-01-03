e2b3b0c6b3c2d6693968abbd4cf727913a6f5f27
thapa-sabin <thapasabin@zohomail.com>
2025-01-03 21:36:23 +0545

    Building a JSON GET endpoint.

--- PYWEB/index.py
+++ PYWEB/index.py
@@ -1,33 +1,17 @@
 import tornado.web
 import tornado.ioloop # Thread that is continuously waiting for result
-
-class basicRequestHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write("Hello, World! This is a Python code executed from the backend.")
+import json
 
 class listRequestHandler(tornado.web.RequestHandler):
     def get(self):
-        self.render("index.html") # The render method should be used to "render" a file to show the client
+        f = open("list.txt", "r")
+        fruits = f.read().splitlines()
+        f.close()
+        self.write(json.dumps(fruits))
 
-class queryParamRequestHandler(tornado.web.RequestHandler):
-    def get(self):
-        num = self.get_argument("num")
-        if (num.isdigit()):
-            r = "odd" if int(num) % 2 else "even"
-            self.write(f"The integer {num} is {r}")
-        else:
-            self.write(f"{num} is not a valid integer.")
-
-class resourceParamRequestHandler(tornado.web.RequestHandler):
-    def get(self, studentName, courseID):
-        self.write(f"Welcome {studentName}, the course you are viewing is {courseID}")
-
-if __name__ == "__main__": # This code should execute only once
+if __name__ == "__main__":
     app = tornado.web.Application([
-        (r"/", basicRequestHandler),
-        (r"/animals", listRequestHandler),
-        (r"/isEven", queryParamRequestHandler),
-        (r"/students/([a-z]+)/([0-9]+)", resourceParamRequestHandler) # Hardcoded requirement for a-z upto infinity and 0-9 upto infinity
+        (r"/list", listRequestHandler)
     ]) # Array of tuples of endpoints that will request handlers
 
     port = 8888
